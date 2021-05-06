2b4c35a0b3a3fdae0a9bd6672a53fbbc68423fc0
inhyeok <inhyeok@localhost>
2021-05-06 10:49:05 +0900

    first commit !!

---
+++ app/src/main/java/edu/handong/csee/java/hw2/Util.java
@@ -0,0 +1,11 @@
+package edu.handong.csee.java.hw2;
+/**
+*   Util is a class that change user's input to UpperCase
+*/
+public class Util {
+
+    static String getUppercaseString(String stringValue) {
+        return stringValue.toUpperCase();
+    }
+    
+}
